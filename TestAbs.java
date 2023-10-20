757641fc14d765c767a55326e8aed6d0f8e26315
Vibha <kiotvibha@gmail.com>
2023-10-20 14:44:16 +0530

    TestAbs.java

--- TestAbs.java
+++ TestAbs.java
@@ -1,13 +1,11 @@
 abstract class TestA{
- abstract void run();{
-        System.out.println("This is Abstract method");
-    }
+ abstract void run();
 }
 class TestAbs extends TestA{
     void run() {
         System.out.println("This is sub class");
     }
-    public static void main(String[] args) {
+public static void main(String[] args) {
 TestA obj=new TestAbs();
 obj.run();
     }
